0ff6164b2dcbab6c1b8a7e54c60dea5368f0ed53
COTOMO <98645428+Orynnx@users.noreply.github.com>
2026-07-11 16:38:19 +0800

    发布 v2.2.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "org.orynnx.outerview"
         minSdk = 36
         targetSdk = 37
-        versionCode = 5
-        versionName = "2.2.0"
+        versionCode = 6
+        versionName = "2.2.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
